8b9bc18f464a51bf7ca0926675e8c695d1fc32e1
luodder <408087878@qq.com>
2016-06-10 22:21:09 +0800

    create index and detail view html

--- blog/urls.py
+++ blog/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+urlpatterns = patterns(' ',
+                       (r'^$', 'blogpost.views.index'),
+                       url(r'^blog/(?p<slug>[^\.]+).html', 'bligpost.views.view_post', name='view_blog_post'),
+                       url(r'^admin/', include(admin.site.urls))
+                       ) + static(settings.STATIC_URL, document_root =settings.STATIC_ROOT)
