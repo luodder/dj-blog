abe64e9ca6054ac27e1827b08bd6b5ef1f17ccb3
luodder <408087878@qq.com>
2016-06-13 22:53:24 +0800

    pytho2 to python3

--- blogpost/models.py
+++ blogpost/models.py
@@ -15,7 +15,7 @@ class Blogpost(models.Model):
     body = models.TextField()
     posted = models.DateField(db_index=True, auto_now_add=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return '%s' % self.title
 
     @permalink
